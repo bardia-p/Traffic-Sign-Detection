5ecc0c45be9b04dcd46e874e5858873aeae4f75b
Bardia Parmoun <bardiaparmoun@gmail.com>
2024-04-04 09:20:08 -0400

    Refactored the code for main.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+import random
+from datetime import datetime
 
 from sign_detection.sign_detector import SignDetector
 
@@ -8,36 +10,44 @@ from template_match.template_matcher import TemplateMatcher
 
 from sign_translator.sign_translator import SignTranslator
 
-image = cv2.imread("test.jpg")
+def process_image(input_file, output_file=""):
+    random.seed(datetime.now().timestamp())
 
-signs = SignDetector().find_signs(image.copy())
+    image = cv2.imread(input_file)
 
-img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    signs = SignDetector().find_signs(image.copy())
 
-translator = SignTranslator()
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-results = dict()
+    translator = SignTranslator()
 
-for sign in signs:
-    top_recogs = Recog().recog_image(sign[0])
+    results = dict()
 
-    matches = TemplateMatcher().locate_sign(img_gray, top_recogs)
+    for sign in signs:
+        top_recogs = Recog().recog_image(sign[0])
 
-    if len(matches) > 0:
-        tm_res = matches[0][1]
-    else:
-        tm_res = -1
+        matches = TemplateMatcher().locate_sign(img_gray, top_recogs)
 
-    print(top_recogs)
-    print(matches)
+        if len(matches) > 0:
+            tm_res = matches[0][1]
+        else:
+            tm_res = -1
 
-    if tm_res != -1:
-        if tm_res not in results:
-            results[tm_res] = sign
+        print(top_recogs)
+        print(matches)
 
-for r in results.keys():
-    sign = results[r]
-    cv2.rectangle(image,(sign[1][0], sign[1][1]), (sign[1][0] + sign[1][2], sign[1][1] + sign[1][3]), (0, 255, 0), 2)
-    cv2.putText(image, translator.get_sign(r), (sign[1][0], sign[1][1]),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        if tm_res != -1:
+            if tm_res not in results:
+                results[tm_res] = sign
 
-cv2.imwrite("result.png", image)
+    for r in results.keys():
+        sign = results[r]
+        cv2.rectangle(image,(sign[1][0], sign[1][1]), (sign[1][0] + sign[1][2], sign[1][1] + sign[1][3]), (0, 255, 0), 2)
+        cv2.putText(image, translator.get_sign(r), (sign[1][0], sign[1][1]),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    if output_file == "":
+        output_file = "processed_signs_" + str(random.randint(1,100)) + ".png"
+
+    cv2.imwrite(output_file, image)
+
+    return output_file
