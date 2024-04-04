c62225b32d5ec3887b5729ff9409e93f9553344c
Bardia Parmoun <bardiaparmoun@gmail.com>
2024-04-04 09:03:33 -0400

    Added a way to get the sign name.

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from neural_network.src.recognize_image import Recog
 
 from template_match.template_matcher import TemplateMatcher
 
+from sign_translator.sign_translator import SignTranslator
+
 image = cv2.imread("test.jpg")
 
 signs = SignDetector().find_signs(image.copy())
 
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+translator = SignTranslator()
 
 results = dict()
 
@@ -34,6 +38,6 @@ for sign in signs:
 for r in results.keys():
     sign = results[r]
     cv2.rectangle(image,(sign[1][0], sign[1][1]), (sign[1][0] + sign[1][2], sign[1][1] + sign[1][3]), (0, 255, 0), 2)
-    cv2.putText(image, str(r), (sign[1][0], sign[1][1]),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(image, translator.get_sign(r), (sign[1][0], sign[1][1]),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
 cv2.imwrite("result.png", image)
